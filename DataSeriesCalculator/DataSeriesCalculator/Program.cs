87517e8741ab80c31ed8c3ad5a98a7eb60c4a87d
MHT <mht@GoApplicate.com>
2016-10-10 22:20:50 +0200

    add comment with wanted calculation

--- DataSeriesCalculator/DataSeriesCalculator/Program.cs
+++ DataSeriesCalculator/DataSeriesCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using DataSeriesCalculator.Calculation.AsCompositePattern;
 using DataSeriesCalculator.DataStructures;
 
@@ -19,15 +20,16 @@ namespace DataSeriesCalculator
 
         private static string CalculateUsingCompositePattern()
         {
+            //((s1 + s2 + s3) * 100 ) + s4
             var s1 = new DataSeriesLeaf(new[] {1, 2, 3, 4});
             var s2 = new DataSeriesLeaf(new[] {4, 3, 2, 1});
             var s3 = new DataSeriesLeaf(new[] {1, 1, 1, 2});
             var s4 = new DataSeriesLeaf(new[] {1, 10, 100, 1000});
 
-            IComponent addingComponentS1S2AndS3 = new AdderComposite(new List<IComponent> {s1, s2, s3}); //6,6,6,7
-            IComponent multiplicationComponent = new MultiplyByConstantComposite(addingComponentS1S2AndS3, 100);//600,600,600,700
-            IComponent addingComponent = new AdderComposite(new List<IComponent> {multiplicationComponent, s4});//601,610,700,1700
-            string resultOfCompositePattern = addingComponent.Calculate().ToString();
+            IComponent addingS1S2AndS3 = new AdderComposite(new List<IComponent> {s1, s2, s3}); //6,6,6,7
+            IComponent multiplyingBy100 = new MultiplyByConstantComposite(addingS1S2AndS3, 100);//600,600,600,700
+            IComponent addingS4 = new AdderComposite(new List<IComponent> {multiplyingBy100, s4});//601,610,700,1700
+            string resultOfCompositePattern = addingS4.Calculate().ToString();
             return resultOfCompositePattern;
         }
     }
